72142a133f7ac463a6a2f3edb7548100e4da2191
gpatsias <georgepatsias@hotmail.co.uk>
2023-08-11 00:08:43 +0300

    props

--- src/index.js
+++ src/index.js
@@ -4,14 +4,43 @@ import ReactDOM from "react-dom";
 // CSS
 import './index.css'
 
+// setup vars
+const firstBook = {
+  img: "https://m.media-amazon.com/images/I/51p2SDOCV9L._SX218_BO1,204,203,200_QL40_ML2_.jpg",
+  author: "Amelia Hepworth",
+  title: "I Love You to the Moon and Back"
+}
+
+const secondBook = {
+  img: "https://images-na.ssl-images-amazon.com/images/I/91vnzZO5yPL._AC_UL300_SR300,200_.jpg",
+  author: "Eric Carle",
+  title: "The Very Hungry Caterpillar"
+}
+
+// const title = "I Love You to the Moon and Back";
+// const author = "Amelia Hepworth";
+// const img = "https://m.media-amazon.com/images/I /51p2SDOCV9L._SX218_BO1,204,203,200_QL40_ML2_.jpg";
+
 function BookList() {
   return (
     <section className="booklist">
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
+      <Book 
+        img={firstBook.img} 
+        title={firstBook.title} 
+        author={firstBook.author} 
+      >
+        <p>Nulla duis labore nulla elit. Dolore cillum minim 
+          anim fugiat eiusmod veniam id ut enim officia 
+          ullamco. Dolore eu ut enim adipisicing id ad et.
+        </p>
+      </Book>
+      <Book 
+        img={secondBook.img} 
+        title={secondBook.title} 
+        author={secondBook.author}
+      >
+        <p>Esse sint eu nulla ex minim mollit anim. Reprehenderit ipsum deserunt pariatur minim reprehenderit nostrud veniam adipisicing et qui. Non et enim non consequat voluptate ut. Tempor deserunt incididunt pariatur sit est voluptate ex id nulla nisi. Esse consectetur cupidatat culpa ullamco cupidatat veniam nulla sit et ad ex sint.</p>
+      </Book>
       <Book />
       <Book />
       <Book />
@@ -20,28 +49,17 @@ function BookList() {
   );
 }
 
-const Book = () => {
+const Book = ({img,title,author,children}) => {
+  // const {img, title, author} = props;
   return (
     <article className="book">
-      <Image />
-      <Title />
-      <Author /> 
+      <img src={img} alt="" />
+      <h1>{title}</h1>
+      <h4>{author}</h4>
+      {children}
     </article>
   );
 };
-
-const Image = () => 
-  <img 
-    src="https://m.media-amazon.com/images/I/51p2SDOCV9L.
-    _SX218_BO1,204,203,200_QL40_ML2_.jpg" 
-    alt=""
-  />
-
-const Title = () => <h1>I Love You to the Moon and Back</h1>;
-const Author = () => 
-  <p style={{color: '#617d98', fontSize: '0.75rem', marginTop: '0.25rem'}}>
-    Amelia Hepworth
-  </p>;
 
 ReactDOM.render(<BookList/>,document.getElementById('root'));
 
